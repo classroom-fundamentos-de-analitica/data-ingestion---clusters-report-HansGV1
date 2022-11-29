29af7ea99178d0b41ca2220df5dc63d0db1bb844
Hans García <82074617+HansGV1@users.noreply.github.com>
2022-11-29 18:36:22 -0500

    Up

--- pregunta.py
+++ pregunta.py
@@ -1,4 +1,4 @@
-# Ingestión de datos - Reporte de cces
+# Ingestión de datos - Reporte de clusteres
 # -----------------------------------------------------------------------------------------
 
 # Construya un dataframe de Pandas a partir del archivo 'c_report.txt', teniendo en
@@ -16,30 +16,32 @@ def ingest_data():
 
     mr = mr[4:]
     c = []
-    cc = [0, 0, 0, '']
+    cluster = [0, 0, 0, '']
 
     for i in mr:
         if re.match('^ +[0-9]+ +', i):
             number, cantidad, porcentaje, *words = i.split()
             
-            cc[0] = int(number)
-            cc[1] = int(cantidad)
-            cc[2] = float(porcentaje.replace(',','.'))
+            cluster[0] = int(number)
+            cluster[1] = int(cantidad)
+            cluster[2] = float(porcentaje.replace(',','.'))
 
             words.pop(0) # Se elimina el carácter '%'
             words = ' '.join(words)
-            cc[3] += words
+            cluster[3] += words
 
         elif re.match('^\n', i) or re.match('^ +$', i):
-            cc[3] = cc[3].replace('.', '') # Se elimina el punto final
-            c.append(cc)
-            cc = [0, 0, 0, '']
+            cluster[3] = cluster[3].replace('.', '') # Se elimina el punto final
+            c.append(cluster)
+            cluster = [0, 0, 0, '']
 
         elif re.match('^ +[a-z]', i):
             words = i.split()
             words = ' '.join(words)
-            cc[3] += ' ' + words
+            cluster[3] += ' ' + words
 
-    f = pd.DataFrame (c, columns = ['cc', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
+    f = pd.DataFrame (c, columns = ['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
     
     return f
+
+print(ingest_data().cluster.to_list())
